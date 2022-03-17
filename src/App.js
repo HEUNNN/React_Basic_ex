6291e6ca1738221754f889f07ec849a17466eb22
leehyeeun <leehe0430@gmail.com>
2022-03-17 18:45:00 +0900

    React Velopert 공통 레이아웃 컴포넌트

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Articles from "./pages/Articles";
 import Article from "./pages/Article";
+import Layout from "./Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profile/:userName" element={<Profile />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile/:userName" element={<Profile />} />
+        </Route>
         <Route path="/articles" element={<Articles />}>
           <Route path=":id" element={<Article />} />
         </Route>
