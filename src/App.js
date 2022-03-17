7fa6ab430035904ea494a9341ddb40b3aa9c28db
leehyeeun <leehe0430@gmail.com>
2022-03-17 17:23:55 +0900

    React velopert URL parameter exam

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <h1>페이지가 변해도 그대로 유지되는 부분</h1>
-      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/profile/:userName" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
