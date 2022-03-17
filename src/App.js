6787e754bc6d00382933dabf5750824639a83b2b
leehyeeun <leehe0430@gmail.com>
2022-03-17 18:33:26 +0900

    React Velopert exam 중첩된 라우트 Outlet Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import Articles from "./pages/Articles";
+import Article from "./pages/Article";
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile/:userName" element={<Profile />} />
+        <Route path="/articles" element={<Articles />}>
+          <Route path=":id" element={<Article />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
